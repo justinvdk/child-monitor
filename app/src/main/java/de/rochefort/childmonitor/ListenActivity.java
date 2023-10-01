3d0cf2444c5bd27d80b7229cecffad265d02594a
edr <github@rochefort.de>
2023-10-01 20:57:42 +0200

    Remove redundant article in license text

--- app/src/main/java/de/rochefort/childmonitor/ListenActivity.java
+++ app/src/main/java/de/rochefort/childmonitor/ListenActivity.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of the Child Monitor.
+ * This file is part of Child Monitor.
  *
  * Child Monitor is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
