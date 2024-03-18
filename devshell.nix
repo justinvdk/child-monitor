c652b9b31feb42243df15ab836243cfa2d6142c9
Justin van der Krieken <justin@vdkrieken.com>
2024-03-19 00:53:35 +0100

    Add nix flake

---
+++ devshell.nix
@@ -0,0 +1,47 @@
+{ pkgs, androidSdk }:
+
+with pkgs;
+
+devshell.mkShell {
+  name = "baby-monitor";
+  motd =
+    let
+      steps = [
+        "./gradlew assembleRelease"
+        "\\\$ANDROID_SDK_ROOT/build-tools/*/zipalign 4 ./app/build/outputs/apk/release/app-release-unsigned.apk ./release-unsigned-aligned.apk"
+        "\\\$ANDROID_SDK_ROOT/build-tools/*/apksigner sign --ks /data/secrets/android.keystore --ks-key-alias baby-monitor --in ./release-unsigned-aligned.apk --out ./release-signed.apk"
+      ];
+    in ''
+      Entered devenv for:
+      baby-monitor - Android-based Child Monitor
+
+      steps: $steps
+
+      Go to ~/Code/baby-monitor to start working.
+
+      Building a new signed apk:
+      1. ${builtins.elemAt steps 0}
+      2. ${builtins.elemAt steps 1}
+      3. ${builtins.elemAt steps 2}
+         password for store in password manager under android.keystore.
+      4. (from somewhere else): rsync --progress woodhouse:Code/child-monitor/release-signed.apk ~/Share/APKs/child-monitor.apk
+
+      copy-pastable:
+      rm -f release-unsigned-aligned.apk release-signed.apk && ${builtins.elemAt steps 0} && ${builtins.elemAt steps 1} && ${builtins.elemAt steps 2} && echo 'Okay, done with all. Now grab the apk and put it somewhere.'
+
+      android-tools are available.'';
+  env = [
+    {
+      name = "GRADLE_OPTS";
+      value = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
+    }
+    {
+      name = "ANDROID_SDK_ROOT";
+      value = "${androidSdk}/libexec/android-sdk";
+    }
+  ];
+  packages = [
+    androidSdk
+    jdk17
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "child-monitor - Android-based Child Monitor.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, devshell, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs) lib;
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
+          overlays = [
+            devshell.overlays.default
+          ];
+        };
+        buildToolsVersion = "34.0.0";
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          buildToolsVersions = [ buildToolsVersion ];
+          #platformVersions = [ "26" "30" "31" "33" "28" ];
+          platformVersions = [ "26" ];
+          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+        };
+        androidSdk = androidComposition.androidsdk;
+      in
+      {
+        devShell = import ./devshell.nix { pkgs = pkgs; androidSdk = androidSdk; };
+      });
+}
